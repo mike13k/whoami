befb1202676765aced5a8476938fcd01235fca3b
mike13k <mikegeorge2012@gmail.com>
2018-04-22 07:40:17 +0200

    works with one known person in view, can only do 20 transactions every minute. it is also slow

---
+++ annotate.py
@@ -0,0 +1,55 @@
+import httplib
+import json
+import urllib
+
+subscription_key = '3ae27aba10974d98a0546726c5ff9a55'
+assert subscription_key
+
+def annotate_image(image):
+    headers = {'Ocp-Apim-Subscription-Key': "3ae27aba10974d98a0546726c5ff9a55",
+               'Content-type': 'application/octet-stream'}
+
+    params = urllib.urlencode({
+        'returnFaceAttributes': 'smile'
+    })
+
+    conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+    conn.request("POST", "/face/v1.0/detect?%s" % params, image, headers)
+
+    response = conn.getresponse("")
+    data = response.read()
+    data = data[1:-1]
+    print(data)
+    if len(data) > 3:
+        face = json.loads(data)
+        if 'error' not in face:
+            attributes = face["faceAttributes"]
+            conn.close()
+            if attributes["smile"] > 0.6:
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+
+    # response = requests.post(face_api_url, params=params, headers=headers, data=image_url)
+    # faces = response.json()
+    #
+    # image_file = BytesIO(requests.get(image_url).content)
+    # image = Image.open(image_file)
+    #
+    # plt.figure(figsize=(8,8))
+    # ax = plt.imshow(image, alpha=0.6)
+    # for face in faces:
+    #     fr = face["faceRectangle"]
+    #     fa = face["faceAttributes"]
+    #     return fa["smile"]
+    # #     origin = (fr["left"], fr["top"])
+    # #     p = patches.Rectangle(origin, fr["width"], \
+    # #                           fr["height"], fill=False, linewidth=2, color='b')
+    # #     ax.axes.add_patch(p)
+    # #     plt.text(origin[0], origin[1], "%d, %d"%(fa["smile"], fa["smile"]), \
+    # #              fontsize=20, weight="bold", va="bottom")
+    # # plt.axis("off")
--- whoami.py
+++ whoami.py
@@ -2,6 +2,13 @@ import face_recognition
 import cv2
 import glob, os
 from os.path import splitext
+import annotate
+import base64
+import io
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 owd = os.getcwd()
 
@@ -16,6 +23,7 @@ pics = glob.glob('*.jpg')
 
 known_face_encodings = []
 known_face_names = []
+smiles = []
 
 # Load sample pictures and learn how to recognize it.
 for pic in pics:
@@ -25,6 +33,7 @@ for pic in pics:
         img_encoding = face_recognition.face_encodings(img)[0]
         known_face_encodings.append(img_encoding)
         known_face_names.append('Prof. Dr. Slim Abdennadher')
+        smiles.append(False)
 
 
 ## Prof. Dr. Ashraf Mansour
@@ -41,6 +50,23 @@ for pic in pics:
         img_encoding = face_recognition.face_encodings(img)[0]
         known_face_encodings.append(img_encoding)
         known_face_names.append('Prof. Dr. Ashraf Mansour')
+        smiles.append(False)
+
+## Michael Khalil
+os.chdir(owd)
+os.chdir('pics/Mike')
+
+pics = glob.glob('*.jpg')
+
+# Load sample pictures and learn how to recognize it.
+for pic in pics:
+    img = face_recognition.load_image_file(pic)
+    encoding = face_recognition.face_encodings(img)
+    if len(encoding) > 0:
+        img_encoding = face_recognition.face_encodings(img)[0]
+        known_face_encodings.append(img_encoding)
+        known_face_names.append('Michael Khalil')
+        smiles.append(False)
 
 
 # Initialize some variables
@@ -53,6 +79,7 @@ color = (0, 0, 0)
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
+    frame = cv2.flip(frame, 1)
 
     # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
@@ -76,11 +103,18 @@ while True:
             if True in matches:
                 first_match_index = matches.index(True)
                 name = known_face_names[first_match_index]
+                # big_frame = cv2.resize(frame, (0, 0), fx=2.0, fy=2.0)
+                # rgb_big_frame = big_frame[:, :, ::-1].copy(order='C')
+                os.chdir(owd)
+                cv2.imwrite('detect.jpg', small_frame)
+                with open('detect.jpg', 'rb') as f:
+                    image_to_annotate = f.read()
+                smiles[first_match_index] = annotate.annotate_image(image_to_annotate)
+                print(smiles[first_match_index])
 
             face_names.append(name)
 
     process_this_frame = not process_this_frame
-
 
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
@@ -93,15 +127,29 @@ while True:
         if name == "Unknown":
             color = (0, 0, 225)
         else:
+            color = (0, 128, 128)
+
+        index = known_face_names.index(name)
+
+        if(smiles[index]):
+
             color = (0, 255, 0)
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), color, cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), color, cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, "Nice Smile!", (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        else:
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), color, cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, 'Hello ' + name + ', I heard you have the best smile', (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
 
     # Display the resulting image
     cv2.imshow('Video', frame)
